9b60342a258a48bc262930b0211c2b606f9f0ca2
thadurivamshi <56868243+thadurivamshi@users.noreply.github.com>
2021-06-14 16:41:13 +0530

    Create  Codeforces Round #709 (Div. 2, based on Technocup 2021 Final Round), problem: (A) Prison Break.cpp

---
+++ Codeforces Round #709 (Div. 2, based on Technocup 2021 Final Round), problem: (A) Prison Break.cpp
@@ -0,0 +1,15 @@
+//https://codeforces.com/problemset/problem/1484/A
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t,a,b;
+    cin >> t;
+    while(t--){
+        cin >> a>>b;
+        cout <<a*b<<endl;
+    }
+    return 0;
+}
